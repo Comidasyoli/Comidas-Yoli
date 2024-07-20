d4c9a3ec2cdbfa825c268b59c86a0775a1482546
Comidasyoli <comidasyolitila@gmail.com>
2024-07-20 08:00:22 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ document.getElementById('whatsappButton').addEventListener('click', function() {
     var whatsappUrl = 'https://wa.me/' + phoneNumber + '?text=' + encodeURIComponent(message);
     window.location.href = whatsappUrl;
 });
+
+document.getElementById('whatsappButtonBottom').addEventListener('click', function() {
+    var phoneNumber = '529999999999'; // Reemplaza con tu número de WhatsApp
+    var message = '¡Hola! Me gustaría hacer un pedido.';
+    var whatsappUrl = 'https://wa.me/' + phoneNumber + '?text=' + encodeURIComponent(message);
+    window.location.href = whatsappUrl;
+});
